24680431c990574bfff75ac12535a1bddbe46049
k.kajiwara <k.kajiwara@brewus.co.jp>
2024-09-05 18:16:07 +0900

    fix plugin view

--- hyperion-compose-plugin/src/main/java/com/github/kakajika/hyperion_compose_plugin/ComposePluginModule.kt
+++ hyperion-compose-plugin/src/main/java/com/github/kakajika/hyperion_compose_plugin/ComposePluginModule.kt
@@ -1,17 +1,18 @@
 package com.github.kakajika.hyperion_compose_plugin
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.currentComposer
 import androidx.compose.ui.platform.ComposeView
 import com.willowtreeapps.hyperion.plugin.v1.HyperionMenu
 import com.willowtreeapps.hyperion.plugin.v1.OverlayContainer
 import com.willowtreeapps.hyperion.plugin.v1.PluginModule
 
 abstract class ComposePluginModule : PluginModule() {
-    protected lateinit var view: ComposeView
+    protected lateinit var view: FrameLayout
 
     protected val overlay: OverlayContainer by lazy { extension.overlayContainer }
     protected val overlayView: View? get() = overlay.overlayView
@@ -21,19 +22,27 @@ abstract class ComposePluginModule : PluginModule() {
     abstract fun PluginViewContent()
 
     override fun createPluginView(layoutInflater: LayoutInflater, parent: ViewGroup): View? {
-        return ComposeView(parent.context).apply {
-            setContent {
-                currentComposer.collectParameterInformation()
-                PluginViewContent()
-            }
+        return FrameLayout(parent.context).apply {
+            id = View.generateViewId()
+            addView(createPluginViewContent(parent.context))
             view = this
         }
     }
 
-    protected fun reloadPluginView() {
-        view.setContent {
-            currentComposer.collectParameterInformation()
-            PluginViewContent()
+    private fun createPluginViewContent(context: Context): View {
+        return ComposeView(context).apply {
+            setContent {
+                PluginViewContent()
+            }
         }
+    }
+
+    protected fun reloadPluginView() {
+        view.removeAllViews()
+        view.addView(createPluginViewContent(context))
+    }
+
+    override fun isStandalone(): Boolean {
+        return false
     }
 }
--- hyperion-measurement-compose/src/main/java/com/github/kakajika/hyperion_measurement_compose/ComposeMeasurementModule.kt
+++ hyperion-measurement-compose/src/main/java/com/github/kakajika/hyperion_measurement_compose/ComposeMeasurementModule.kt
@@ -3,34 +3,19 @@ package com.github.kakajika.hyperion_measurement_compose
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.mutableStateOf
 import com.github.kakajika.hyperion_compose_plugin.ComposePluginModule
 import com.github.kakajika.hyperion_measurement_compose.overlay.MeasurementOverlayView
-import com.github.kakajika.hyperion_measurement_compose.theme.HyperionBlue
-import com.github.kakajika.hyperion_measurement_compose.theme.HyperionText
 import com.willowtreeapps.hyperion.plugin.v1.MenuState
 import com.willowtreeapps.hyperion.plugin.v1.OnMenuStateChangedListener
 import com.willowtreeapps.hyperion.plugin.v1.OnOverlayViewChangedListener
 
-class ComposeMeasurementModule : ComposePluginModule(),
+internal class ComposeMeasurementModule : ComposePluginModule(),
     OnOverlayViewChangedListener,
     OnMenuStateChangedListener {
+    private val isSelected = mutableStateOf<Boolean>(false)
+
     override fun onCreate() {
         overlay.addOnOverlayViewChangedListener(this)
         hyperionMenu?.addOnMenuStateChangedListener(this)
@@ -38,40 +23,15 @@ class ComposeMeasurementModule : ComposePluginModule(),
 
     @Composable
     override fun PluginViewContent() {
-        val tint = if (view.isSelected) HyperionBlue else HyperionText
-        Row(
-            modifier = Modifier
-                .height(131.dp)
-                .fillMaxWidth()
-                .clickable(onClick = ::onClick)
-                .padding(dimensionResource(id = com.willowtreeapps.hyperion.plugin.R.dimen.hype_plugin_padding)),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Image(
-                modifier = Modifier
-                    .padding(end = 28.dp)
-                    .size(dimensionResource(id = com.willowtreeapps.hyperion.plugin.R.dimen.hype_plugin_icon_size)),
-                painter = painterResource(id = R.drawable.ic_measurement),
-                colorFilter = ColorFilter.tint(tint),
-                contentDescription = "Compose Measurement",
-            )
-            Column {
-                Text(
-                    text = "Compose Measurement",
-                    fontSize = 16.sp,
-                    color = tint,
-                )
-                Text(
-                    text = "Tap Composables to measure the distances between them.",
-                    color = tint,
-                )
-            }
-        }
+        ComposeMeasurementPluginView(
+            isSelected = isSelected.value,
+            onClick = ::onClick,
+        )
     }
 
     override fun createPluginView(layoutInflater: LayoutInflater, parent: ViewGroup): View? {
         return super.createPluginView(layoutInflater, parent).also {
-            view.isSelected = OVERLAY_TAG == overlayView?.tag
+            isSelected.value = OVERLAY_TAG == overlayView?.tag
         }
     }
 
@@ -80,12 +40,8 @@ class ComposeMeasurementModule : ComposePluginModule(),
         hyperionMenu?.removeOnMenuStateChangedListener(this)
     }
 
-    override fun isStandalone(): Boolean {
-        return false
-    }
-
     private fun onClick() {
-        if (!view.isSelected) {
+        if (!isSelected.value) {
             val newOverlay = MeasurementOverlayView(context).apply {
                 tag = OVERLAY_TAG
             }
@@ -96,13 +52,15 @@ class ComposeMeasurementModule : ComposePluginModule(),
     }
 
     override fun onOverlayViewChanged(v: View?) {
-        view.isSelected = OVERLAY_TAG == v?.tag
-        reloadPluginView()
+        isSelected.value = OVERLAY_TAG == v?.tag
     }
 
     override fun onMenuStateChanged(menuState: MenuState) {
+        if (menuState == MenuState.OPEN) {
+            reloadPluginView()
+        }
         if (menuState == MenuState.CLOSE) {
-            if (view.isSelected) {
+            if (isSelected.value) {
                 val newOverlay = MeasurementOverlayView(context).apply {
                     tag = OVERLAY_TAG
                 }
